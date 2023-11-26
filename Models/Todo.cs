8c508572d3d21fba8f6a1b07280388cd73b8392c
samzzii <nasstromsamuel@icloud.com>
2023-11-26 22:41:24 +0100

    added CRUD for Todo-app

---
+++ Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public PriorityPicker Priority { get; set; }
+    }
+
+    public enum PriorityPicker
+    {
+        MustDo,
+        CouldDo,
+        DoNotHaveTo
+    }
+}
